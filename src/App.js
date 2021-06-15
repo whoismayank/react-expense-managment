25a224a6c4b77c3795cb91f1e0fdd15ca80119fe
mahendrachoudharyin <51509905+mahendrachoudharyin@users.noreply.github.com>
2021-06-15 21:38:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expenses from './components/Expenses/Expenses';
+const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,6 @@
+import './Card.css';
+const Card = (props) =>{
+    const classes = 'Card '+props.className;
+    return <div className={classes}>{props.children}</div>
+}
+export default Card;
